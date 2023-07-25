f427121874a090b87fe1fe312fad576c298aff7a
skmanoj322 <sk.manoj322@gmail.com>
2023-07-25 23:06:23 +0530

    remover dist

--- dist/routes/useRoutes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const UserDemo_1 = require("../controllers/UserDemo");
-const router = (0, express_1.Router)();
-router.get("users/", UserDemo_1.getUser);
